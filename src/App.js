02a18c09ab5e9ce16fd6a59793b13cd0358ff1e0
unknown <kajotonis@gmail.com>
2022-06-05 22:34:07 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import Homepage from "./components/Homepage";
-import { Route, Routes } from "react-router-dom";
-import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
-import Men from "./components/pages/Men";
-import Scouting from "./components/pages/Scouting";
-import Women from "./components/pages/Women";
+
 
 function App() {
   return (
      <div>
-       <Routes> 
-         <Route path = '/' element = {<Homepage/>} />
-         <Route path="/Women"   element = {<Women />}/>
+         <Homepage/>
           
-         <Route path="/Men" element = {<Men />} />
-         
-         <Route path="/Scouting" element = {<Scouting />} />
-        
-         <Route path="/About" element = {<About/>} />
-              
-         
-         <Route path="/Contact" element = {<Contact />} />
-       
-        </Routes>
      </div>
   );
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,12 @@
-import Menu from "../Menu";
+import Header from "../Header";
 
 const About = () => {
     return ( 
-        <div className="bg-[#F8DBCB]">
-       <Menu />
+        <div className="bg-[#F8DBCB]"> 
+        <Header />
+        <h1 className="mt-10 text-4xl text-center"> ABOUT US</h1>
+        <hr className="border-1 border-black mt-4"/>
+        <p className="mt-10 mx-3">Yanga Model Management is a modelling agency located in Lagos, Southwest Nigeria specializing in the management and marketing of homegrown fashion, runway and commercial models with the aim of nurturing long term and lucrative careers for our models by supporting and giving them the space to grow. Our aim is to be one of the most succesful and respected modelling agencies in Nigeria. Box models prides itselft in scouting raw, unique and talented models. Excellent knowledge of the modelling industry and good understanding of clients enables us to provide and tailored service to our clients.</p>
         </div>
         
      );
