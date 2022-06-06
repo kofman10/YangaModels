a94134c08cf05a846d222ab4c9144ecd7bb7c9e7
unknown <kajotonis@gmail.com>
2022-06-06 16:49:27 +0100

    commit

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,8 +3,29 @@ import Header from "../Header";
 
 const Contact = () => {
     return (
-        <div className="bg-[#F8DBCB]">
+        <div className="bg-[rgb(248,219,203)] h-screen w-full">
         <Header />
+         <h1 className="uppercase "> Contact </h1>
+         <hr className="border-1 border-black"/>
+         <section className="flex flex-col gap-10 justify-center h-full items-center">
+             <div>
+                 <div className="text-center">Mail</div>
+                 <div className="text-center"> info@yangamodels.com</div>
+             </div>
+             <div>
+             <div className="text-center">Phone</div>
+                 <div className="text-center"> +2348145896745</div>
+             </div>
+             <div>
+             <div className="text-center">Follow us on</div>
+                 <div className="text-center"> Instagram</div>
+                 <div className="text-center"> twitter</div>
+                 <div className="text-center"> facebook</div>
+             </div>
+             <div>
+             <div className="text-center">whatsapp</div>
+             </div>
+         </section>
          </div>
       );
 }
