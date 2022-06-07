e6336ea6d989833cc69fdbe3f108b48c8578d5ad
unknown <kajotonis@gmail.com>
2022-06-07 12:37:31 +0100

    commit

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import {Link} from 'react-router-dom'
 
 const Homepage = () => {
     return ( 
@@ -8,7 +9,7 @@ const Homepage = () => {
 <h1 className="text-center text-3xl mt-10 mb-10">WE ARE COMING SOON</h1>
 <p className="text-center">We are working hard to give you the best experience</p>
 <p className="text-center mt-7 ">Yanga Model Management is a modelling agency located in Lagos, Southwest Nigeria specializing in the management and marketing of homegrown fashion, runway and commercial models with the aim of nurturing long term and lucrative careers for our models by supporting and giving them the space to grow.</p>
-<p><button href="#" className="mt-4 underline ml-2 ">Become a YangaModel</button></p>
+<Link to="/Scouting" className="mt-4 underline ml-2 ">Become a YangaModel</Link>
 
         </div>
        
