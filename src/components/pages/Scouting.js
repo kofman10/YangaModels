dc2d5814c5a17d438a30a62b8955bf1facac1195
unknown <kajotonis@gmail.com>
2022-06-07 12:57:52 +0100

    commit

--- src/components/pages/Scouting.js
+++ src/components/pages/Scouting.js
@@ -14,13 +14,13 @@ if we believe that you have the right characteristics to become a Yanga Model.  
              <div className="flex flex-col justify-center gap-3 mx-5"> 
              <input name="firstname" placeholder="FIRSTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
              <input name="firstname" placeholder="LASTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="firstname" placeholder="DATE OF BIRTH" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="firstname" placeholder="EMAIL" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="firstname" placeholder="MOBILE NUMBER" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="firstname" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="firstname" placeholder="CITY" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="firstname" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="firstname" placeholder="TALENT/HOBBIES" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+             <input name="date of birth" placeholder="DATE OF BIRTH" type = 'date' className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+             <input name="email" placeholder="EMAIL" type="email" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+             <input name="Mobile Number" placeholder="MOBILE NUMBER" type="number" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+             <input name="address" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+             <input name="city" placeholder="CITY" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+             <input name="Instagram" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+             <input name="Talent/Hobbies" placeholder="TALENT/HOBBIES" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
 
                   </div>
            <div className="flex items-center ml-5 mt-5"> 
@@ -31,6 +31,9 @@ if we believe that you have the right characteristics to become a Yanga Model.  
                <input className="ml-1 h-4 w-4" type="radio" name="sex" id="male-radio" />
            </div>
              <span className="mt-10">MEASUREMENT</span>
+             <div className="">
+
+             </div>
          </form>
         </div>
       );
