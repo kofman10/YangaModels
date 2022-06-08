29ed65ca128e07f75623c1ba94a881142c891c93
unknown <kajotonis@gmail.com>
2022-06-08 21:29:17 +0100

    commit

--- src/components/pages/Form.js
+++ src/components/pages/Form.js
@@ -12,26 +12,31 @@ const Form = () => {
     <input name="Talent/Hobbies" placeholder="TALENT/HOBBIES" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
 
          </div>
-  <div className="flex items-center ml-5 mt-5"> 
+  <div className="flex items-center ml-5 mt-5 mb-5"> 
       <span className="mr-5">SEX :</span>
       <span >FEMALE</span>
       <input className="mr-2 ml-1 h-4 w-4" type="radio" name="sex" id="female-radio" />
       <span>MALE</span>
       <input className="ml-1 h-4 w-4" type="radio" name="sex" id="male-radio" />
   </div>
-    <span className="mt-10">MEASUREMENT</span>
-    <div className="grid grid-rows-4 grid-flow-col gap-2">
-    <input name="Mobile Number" placeholder="MOBILE NUMBER" type="number" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-    <input name="address" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-    <input name="city" placeholder="CITY" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-    <input name="Instagram" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-    <input name="Mobile Number" placeholder="MOBILE NUMBER" type="number" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-    <input name="address" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-    <input name="city" placeholder="CITY" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-    <input name="Instagram" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-   
 
-    </div>
+    <span className=" ml-5">MEASUREMENT</span>
+
+    <div className="grid grid-cols-2 gap-2 w-full mt-5 mb-5">
+    <input name="Mobile Number" placeholder="MOBILE NUMBER" type="number" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5"/>
+    <input name="address" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5"/>
+    <input name="city" placeholder="CITY" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5"/>
+    <input name="Instagram" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5"/>
+    <input name="Mobile Number" placeholder="MOBILE NUMBER" type="number" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5"/>
+    <input name="address" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5"/>
+    <input name="city" placeholder="CITY" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5"/>
+    <input name="Instagram" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5"/>
+     </div>
+
+     <span className="ml-5">PHOTO & VIDEO SUBMISSION </span>
+   <div className="flex flex-col gap-2 mx-5 mt-2">
+    <div className="border border-black h-[250px] w-3/4"></div>
+   </div>
 </form> );
 }
  
