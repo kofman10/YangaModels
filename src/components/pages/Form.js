dd4e6bbab268b8b98dd7bfe1fa905ef89e98e336
unknown <kajotonis@gmail.com>
2022-06-08 12:26:01 +0100

    commit

---
+++ src/components/pages/Form.js
@@ -0,0 +1,28 @@
+const Form = () => {
+    return (   <form action="" >
+    <div className="flex flex-col justify-center gap-3 mx-5 "> 
+    <input name="firstname" placeholder="FIRSTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+    <input name="lastname" placeholder="LASTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+    <input name="date of birth" placeholder="DATE OF BIRTH(DD-MM-YY)" type="number" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+    <input name="email" placeholder="EMAIL" type="email" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+    <input name="Mobile Number" placeholder="MOBILE NUMBER" type="number" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+    <input name="address" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+    <input name="city" placeholder="CITY" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+    <input name="Instagram" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+    <input name="Talent/Hobbies" placeholder="TALENT/HOBBIES" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
+
+         </div>
+  <div className="flex items-center ml-5 mt-5"> 
+      <span className="mr-5">SEX :</span>
+      <span >FEMALE</span>
+      <input className="mr-2 ml-1 h-4 w-4" type="radio" name="sex" id="female-radio" />
+      <span>MALE</span>
+      <input className="ml-1 h-4 w-4" type="radio" name="sex" id="male-radio" />
+  </div>
+    <span className="mt-10">MEASUREMENT</span>
+    <div className="">
+    </div>
+</form> );
+}
+ 
+export default Form;
--- src/components/pages/Scouting.js
+++ src/components/pages/Scouting.js
@@ -1,5 +1,5 @@
 import Header from "../Header";
-
+import Form from "./Form";
 
 const Scouting = () => {
     return (
@@ -11,30 +11,7 @@ Do you have a story to tell? Please complete all the sections below and we will 
 if we believe that you have the right characteristics to become a Yanga Model.  </p>
 
 <span>INFO</span>
-         <form action="" >
-             <div className="flex flex-col justify-center gap-3 mx-5 "> 
-             <input name="firstname" placeholder="FIRSTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="firstname" placeholder="LASTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input type="date" min='05-01-1980' name="date of birth" id="date" className="placeholder-black bg-inherit border border-black  p-2"/>
-             <input name="email" placeholder="EMAIL" type="email" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="Mobile Number" placeholder="MOBILE NUMBER" type="number" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="address" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="city" placeholder="CITY" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="Instagram" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-             <input name="Talent/Hobbies" placeholder="TALENT/HOBBIES" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2"/>
-
-                  </div>
-           <div className="flex items-center ml-5 mt-5"> 
-               <span className="mr-5">SEX :</span>
-               <span >FEMALE</span>
-               <input className="mr-2 ml-1 h-4 w-4" type="radio" name="sex" id="female-radio" />
-               <span>MALE</span>
-               <input className="ml-1 h-4 w-4" type="radio" name="sex" id="male-radio" />
-           </div>
-             <span className="mt-10">MEASUREMEN</span>
-             <div className="">
-             </div>
-         </form>
+       <Form />
         </div>
       );
 }
