b6666b750e331bf808637a12982d3d68e6504c87
unknown <kajotonis@gmail.com>
2022-06-09 12:14:36 +0100

    commit

--- src/components/pages/Form.js
+++ src/components/pages/Form.js
@@ -82,7 +82,7 @@ fashion shoot.</li>
 </ul>
  </section>
 
- <button type="submit" className="border bg-[#B4917E] uppercase font-bold rounded-md ml-5 py-3 px-5 mt-5"> Submit application</button>
+ <button type="submit" className="border bg-[#B4917E] uppercase font-bold rounded-md ml-5 py-3 px-5 mt-5 mb-10"> Submit application</button>
 </form> );
 }
  
