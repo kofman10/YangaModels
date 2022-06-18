3f99d14b4c9fbccad42bd39b950203413322711d
unknown <kajotonis@gmail.com>
2022-06-18 23:52:42 +0100

    commit

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,15 +1,14 @@
 import Header from "./Header";
-import {Link} from 'react-router-dom'
 
 const Homepage = () => {
     return ( 
-        <section className="bg-[#F8DBCB] h-screen w-full">
+        <section className="bg-[#F8DBCB] min-h-screen w-full">
   <Header />
  <main className="mx-2"> 
 <h1 className="text-center text-3xl mt-10 mb-10">WE ARE COMING SOON</h1>
 <p className="text-center">We are working hard to give you the best experience</p>
 <p className="text-center mt-7 pb-10">Yanga Model Management is a modelling agency located in Lagos, Southwest Nigeria specializing in the management and marketing of homegrown fashion, runway and commercial models with the aim of nurturing long term and lucrative careers for our models by supporting and giving them the space to grow.</p>
-<Link to="/Scouting" className=" underline ml-2">Become a YangaModel</Link>
+<a href ="/Scouting" className=" underline ml-2">Become a YangaModel</a>
 </main>
         </section>
        
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,16 @@
 import { XIcon } from '@heroicons/react/outline';
 import React from 'react';
- import {  Link } from 'react-router-dom';
 const Menu = ({showMenu,active}) => {
   return ( 
            
               <div className={active ? 'fixed transition animate-slide-in  uppercase bg-[#F8DBCB] backdrop-blur-lg md:hidden' : 'hidden'}>
                   <div className='flex justify-end'> <XIcon onClick={showMenu} className='cursor-pointer w-6 h-6 mt-2 mr-5'/> </div> 
                   <ul className='flex-col flex items-center space-y-8 p-8 h-screen w-screen text-4xl mt-16'> 
-                    <li><Link to='/Women'>women</Link></li>
-                    <li><Link to='/Men'>men</Link></li>
-                    <li><Link to='/Scouting'>scouting</Link></li>
-                    <li><Link to= '/About'>About us</Link></li>
-                    <li><Link to='/Contact'>Contact</Link></li>
+                    <li><a href='/Women'>women</a></li>
+                    <li><a href='/Men'>men</a></li>
+                    <li><a href='/Scouting'>scouting</a></li>
+                    <li><a href= '/About'>About us</a></li>
+                    <li><a href='/Contact'>Contact</a></li>
                     </ul>  
                   
                 </div>
