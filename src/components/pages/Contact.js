88d86b5d04739a693dcf6cd4ad3688939eb775d4
unknown <kajotonis@gmail.com>
2022-06-21 01:29:45 +0100

    commit

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -10,21 +10,21 @@ const Contact = () => {
          <hr className="border-1 border-black mt-4"/>
          <section className="flex flex-col space-y-10 justify-center my-10 items-center uppercase">
              <div>
-                 <p className="text-center opacity-90">Mail</p>
+                 <p className="text-center">Mail</p>
                  <a href="mailto:info@yangamodels.com" className="text-center font-semibold"> info@yangamodels.com</a>
              </div>
              <div>
-             <p className="text-center opacity-90">Phone</p>
+             <p className="text-center ">Phone</p>
                  <p className="text-center font-semibold"> +2348137071908</p>
              </div>
              <div>
-             <p className="text-center opacity-90">Follow us on</p>
+             <p className="text-center ">Follow us on</p>
                  <p href="/" className="text-center font-semibold"> Instagram</p>
                  <p href="/" className="text-center font-semibold"> twitter</p>
                  <p href="/" className="text-center font-semibold"> facebook</p>
              </div>
              <div>
-                 <p className="text-center opacity-90">Message us on</p>
+                 <p className="text-center ">Message us on</p>
              <p href="/" className="text-center font-semibold">whatsapp</p >
              </div>
          </section>
