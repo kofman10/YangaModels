11c89a94629e9834a2d04eb4a4e6f7180bc30290
unknown <kajotonis@gmail.com>
2022-06-22 21:36:02 +0100

    commit

--- src/components/pages/Scouting.js
+++ src/components/pages/Scouting.js
@@ -5,10 +5,9 @@ const Scouting = () => {
     return (
         <div className="bg-[#F8DBCB] h-full  w-full">
             <Header />
-       <h1 className="ml-5 text-[22px] font-semibold font-fraunces mt-3 mb-4 uppercase ">Become a yangamodel </h1>
-       <p className="mx-5 mb-5  tracking-widest leading-relaxed">There is a story behind every face at Yanga Models.
-Do you have a story to tell? Please complete all the sections below and we will get in contact with you
-if we believe that you have the right characteristics to become a Yanga Model.  </p>
+       <h1 className="ml-5 text-[22px] font-semibold font-fraunces mt-3 mb-4 uppercase ">Become a Yanga model </h1>
+       <p className="mx-5 mb-5  tracking-widest leading-relaxed">Think Yanga Model is a family you want to be part of?
+Kindly complete all the sections below and we will get in contact with you if we believe that you have the right characteristics to part of the family. </p>
 
 <p  className="ml-5 mb-5 font-semibold">INFO</p >
        <Form />
