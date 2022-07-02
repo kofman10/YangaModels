74e207a1ee62e0795b0b1044b196c73ed006f555
unknown <kajotonis@gmail.com>
2022-07-03 04:46:31 +0800

    commit

--- src/components/pages/Form.js
+++ src/components/pages/Form.js
@@ -15,21 +15,25 @@ const Form = () => {
     const [imagee, setImagee] = useState();
     const [imageee, setImageee] = useState();
     const [imageeee, setImageeee] = useState();
-    const [progress, setProgress] = useState();
- const [message, setMessage] = useState('');
+    const [url, setUrl] = useState();
+    const [urll, setUrll] = useState();
+    const [urlll, setUrlll] = useState();
+    const [urllll, setUrllll] = useState();
 
-  const { register, watch, handleSubmit, formState: { errors,  }, reset} = useForm();
+
+    const [progress, setProgress] = useState();
+    const [message, setMessage] = useState('');
+  const { register, watch, handleSubmit, formState: { errors  }, reset} = useForm();
    
   const sex = watch('sex')
 
-  const onSubmit = async (data) => {
-       const name =new Date().getTime + image.name;
+  const onSubmit = async  (data) => {
        const storageRef = ref(storage, image.name);
        const uploadTask = uploadBytesResumable(storageRef, image)
 
        uploadTask.on('state_changed', (snapshot) => {
-        const progress =(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setProgress(progress);
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progress) ;
         switch(snapshot.state) {
           case 'paused':
           console.log('upload is paused');
@@ -44,102 +48,135 @@ const Form = () => {
         console.log(error)
        }, 
        () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setUserData((prev) => ({...prev,img: downloadURL}))
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+           setUrl(downloadURL)
+          console.log(url)
         })
-       }
+       }, 
+       ); 
+
+       const storageRe = ref(storage, imagee.name);
+       const uploadTas = uploadBytesResumable(storageRe, imagee)
+
+       uploadTas.on('state_changed', (snapshot) => {
+       
+        switch(snapshot.state) {
+          case 'paused':
+          console.log('upload is paused');
+          break;
+          case 'running':
+            console.log('upload is running');
+            break;
+            default:
+              break;
+        }
+       }, (error) => {
+        console.log(error)
+       }, 
+       () => {
+      getDownloadURL(uploadTas.snapshot.ref).then((downloadURL) => {
+           setUrll(downloadURL)
+          console.log(urll)
+        })
+       }, 
        );
 
-       await addDoc(collection(db, 'applicants'),{
-        ...userData, 
-        timestamp: serverTimestamp()
-       })     
+       const storageR = ref(storage, imageee.name);
+       const uploadTa = uploadBytesResumable(storageR, imageee)
+
+       uploadTa.on('state_changed', (snapshot) => {
+       
+        switch(snapshot.state) {
+          case 'paused':
+          console.log('upload is paused');
+          break;
+          case 'running':
+            console.log('upload is running');
+            break;
+            default:
+              break;
+        }
+       }, (error) => {
+        console.log(error)
+       }, 
+       () => {
+      getDownloadURL(uploadTa.snapshot.ref).then((downloadURL) => {
+           setUrlll(downloadURL)
+          console.log(urlll)
+        })
+       }, 
+       );
+
+       const storag = ref(storage, imageeee.name);
+       const uploadT = uploadBytesResumable(storag, imageeee)
+
+       uploadT.on('state_changed', (snapshot) => {
+       
+        switch(snapshot.state) {
+          case 'paused':
+          console.log('upload is paused');
+          break;
+          case 'running':
+            console.log('upload is running');
+            break;
+            default:
+              break;
+        }
+       }, (error) => {
+        console.log(error)
+       }, 
+       () => {
+      getDownloadURL(uploadT.snapshot.ref).then((downloadURL) => {
+           setUrllll(downloadURL)
+          console.log(urllll)
+        })
+       }, 
+       );
+
+
+        
+       
+    addDoc(collection(db, 'applicants'), {
+    ...data,
+    timestamp: serverTimestamp(),
+    img1: url,
+    img2: urll,
+    img3: urlll,
+    img4 : urllll
+   })
+   
 
      console.log(data);
      setMessage('Thank you, your application has been successfully submitted')
-
-     reset(userData.firstName = '', 
-     userData.lastName = '',
-     userData.phone = '',
-     userData.email = '',
-     userData.address = '',
-     userData.city = '',
-     userData.dateofbirth  = '',
-     userData.instagram = '',
-     userData.talent = '',
-     userData.height = '',
-     userData.mheight = '',
-     userData.weight = '',
-     userData.mweight = '',
-     userData.waist = '',
-     userData.dressSize = '',
-     userData.eyeColour = '',
-     userData.shoeSize = '',
-     userData.inseam = '',
-     userData.suitSize = '',
-     userData.hips = '',
-     userData.bust = ''
+     
+     reset(
+     
      )
 
      setImage(null);
      setImagee(null);
      setImageee(null);
      setImageeee(null);
-     console.log('image: ', image);
+     
   }
    
-  const [userData, setUserData] = useState({
-    firstName: '',
-    lastName: '',
-    phone: '',
-    email: '',
-    address: '',
-    city: '',
-    dateofbirth: '',
-    instagram: '',
-    talent: '',
-    height: '',
-    mheight: '',
-    weight:'',
-    mweight:'',
-    bust: '',
-    waist: '',
-    mwaist: '',
-    hips: '',
-    dressSize: '',
-    eyeColour: '',
-    meyeColour: '',
-    shoeSize: '',
-    mshoeSize: '',
-    chest: '',
-    suitSize: '',
-    inseam: '',
 
-  });
-
-  let name, value;
-  const postUserData = (event) => {
-    name = event.target.name;
-    value = event.target.value;
-
-    setUserData({...userData, [name]: value});
-  };
-
+ 
     return (   
       <> 
     <form  onSubmit = {handleSubmit(onSubmit)} className = 'md:w-3/4'>
     <div className="flex flex-col justify-center  mx-5 md:grid md:grid-cols-2 md:gap-5  md:mt-5 md:mb-5"> 
-    <input required   name="firstName" placeholder="FIRSTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" value = {userData.firstName} onChange = { postUserData}/>
+    <input required   name="firstName" placeholder="FIRSTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0"  {...register("firstName", { required: true })}/>
 
-    <input  required name="lastName" placeholder="LASTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" value = {userData.lastName} onChange = { postUserData}/>
-    <input  required name="dateofbirth" placeholder="DATE OF BIRTH" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" value = {userData.dateofbirth} onChange = { postUserData}/>
-    <input  {...register("email", { required: true, pattern:{value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/} })} name="email" placeholder="EMAIL" type="email" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" value = {userData.email} onChange = { postUserData}/><div className="lg:hidden">{errors.email && <span className="text-red-800">please enter a valid email</span>}
+    <input  required name="lastName" placeholder="LASTNAME" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" {...register('lastName')} />
+    <input  required name="dateofbirth" placeholder="DATE OF BIRTH" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0"{...register('dateofbirth')}  />
+    <input  {...register("email", { required: true, pattern:{value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/} })} name="email" placeholder="EMAIL" type="email" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" /><div className="lg:hidden">{errors.email && <span className="text-red-800">please enter a valid email</span>}
 </div>
-    <input  required name="phone" placeholder="MOBILE NUMBER" type='' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" value = {userData.phone} onChange = { postUserData}/>
-    <input  required name="address" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" value = {userData.address} onChange = { postUserData}/>
-    <input  required name="city" placeholder="CITY" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" value ={userData.city} onChange = {postUserData}/>
-    <input  required name="instagram" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" value = {userData.instagram} onChange = { postUserData}/>
-    <input  required name="talent" placeholder="TALENT/HOBBIES" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" value = {userData.talent} onChange = { postUserData}/>
+    <input  required name="phone" placeholder="MOBILE NUMBER" type='' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" {...register('phone')}/>
+    <input  required name="address" placeholder="ADDRESS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" {...register('phone')} />
+    <input  required name="city" placeholder="CITY" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0"/>
+    <input  required name="instagram" placeholder="INSTAGRAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" {...register('instagram')} />
+    <input  required name="talent" placeholder="TALENT/HOBBIES" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mt-2 md:mt-0" {...register('talent')}/>
     
          </div>
   <div className="flex items-center ml-5 mt-5 mb-5"> 
@@ -153,14 +190,15 @@ const Form = () => {
     <span className=" ml-5 font-semibold">MEASUREMENT</span>
     <div>
       { sex === 'female' && (  <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-7 w-full md:w-3/4 mt-5 mb-5">
-    <input  required name="height" placeholder="HEIGHT" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5" value = {userData.height}   onChange = { postUserData}/>
-    <input  required name="weight" placeholder="WEIGHT" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5 md:mr-0" value = {userData.weight} onChange = { postUserData}/>
-    <input  required name="bust" placeholder="BUST" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5 md:ml-0" value = {userData.bust} onChange = { postUserData}/>
-    <input  required name="waist" placeholder="WAIST" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5" value = {userData.waist} onChange = { postUserData}/>
-    <input  required name="hips" placeholder="HIPS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5" value = {userData.hips} onChange = { postUserData}/>
-    <input  required name="dressSize" placeholder="DRESS SIZE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5 md:mr-0" value = {userData.dressSize} onChange = { postUserData}/>
-    <input  required name="eyeColour" placeholder="EYE COLOUR" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5 md:ml-0" value = {userData.eyeColour} onChange = { postUserData}/>
-    <input  required name="shoeSize" placeholder="SHOE SIZE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5" value = {userData.shoeSize} onChange = { postUserData}/>
+    <input  required name="height" placeholder="HEIGHT" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5" {...register('height')}/>
+    <input  required name="weight" placeholder="WEIGHT" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5 md:mr-0" {...register('weight')}/>
+    <input  required name="bust" placeholder="BUST" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5 md:ml-0" {...register('bust')}/>
+    <input  required name="waist" placeholder="WAIST" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5" {...register('waist')}/>
+    <input  required name="hips" placeholder="HIPS" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5" {...register('hips')}/>
+    <input  required name="dressSize" placeholder="DRESS SIZE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5 md:mr-0" {...register('dressSize')}/>
+    <input  required name="eyeColour" placeholder="EYE COLOUR" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5 md:ml-0" {...register('eyeColour')}/>
+    <input  required name="shoeSize" placeholder="SHOE SIZE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5"
+    {...register('shoeSize')} />
      </div>
 ) }
    
@@ -168,14 +206,14 @@ const Form = () => {
    
    <div>
     {sex === 'male' && ( <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-7 w-full md:w-3/4 mt-5 mb-5">
-    <input  required name="height" placeholder="HEIGHT" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5" value = {userData.mheight} onChange = { postUserData}/>
-    <input  required name="weight" placeholder="WEIGHT" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5 md:mr-0" value = {userData.mweight} onChange = { postUserData}/>
-    <input  required name="chest" placeholder="CHEST" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5 md:ml-0" value = {userData.chest} onChange = { postUserData}/>
-    <input  required name="waist" placeholder="WAIST" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5" value = {userData.mwaist} onChange = { postUserData}/>
-    <input  required name="suitSize" placeholder="SUIT SIZE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5" value = {userData.suitSize} onChange = { postUserData}/>
-    <input  required name="inseam" placeholder="INSEAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5 md:mr-0" value = {userData.inseam} onChange = { postUserData}/>
-    <input  required name="eyeColour" placeholder="EYE COLOUR" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5 md:ml-0" value = {userData.meyeColour} onChange = { postUserData}/>
-    <input  required name="shoeSize" placeholder="SHOE SIZE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5" value = {userData.mshoeSize} onChange = { postUserData}/>
+    <input  required name="height" placeholder="HEIGHT" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5" {...register('height')}/>
+    <input  required name="weight" placeholder="WEIGHT" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5 md:mr-0" {...register('weight')}/>
+    <input  required name="chest" placeholder="CHEST" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5 md:ml-0" {...register('chest')}/>
+    <input  required name="waist" placeholder="WAIST" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5" {...register('waist')}/>
+    <input  required name="suitSize" placeholder="SUIT SIZE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5" {...register('suitSize')}/>
+    <input  required name="inseam" placeholder="INSEAM" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5 md:mr-0" {...register('inseam')}/>
+    <input  required name="eyeColour" placeholder="EYE COLOUR" type='text' className="placeholder-black bg-inherit border border-black focus:outline-none p-2 ml-5 md:ml-0" {...register('eyeColour')}/>
+    <input  required name="shoeSize" placeholder="SHOE SIZE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mr-5" {...register('shoeSize')}/>
      </div>)}
 
   
@@ -186,15 +224,14 @@ const Form = () => {
      <span className="ml-5 font-semibold">PHOTO & VIDEO SUBMISSION </span>
      <div className="flex flex-col space-y-3 mx-5 mt-2">
 
-      <InnerUpload1 image = {image} setImage = {setImage} />
+      <InnerUpload1 image = {image} setImage = {setImage} props  />
       <InnerUpload2 imagee = {imagee} setImagee = {setImagee} />
       <InnerUpload3 imageee = {imageee} setImageee = {setImageee} />
       <InnerUpload4 imageeee = {imageeee} setImageeee = {setImageeee} />
-
        </div>
    <p className="mx-5 mt-5  tracking-wide">We want to know more about you as a person,a funny story  or something most people
 dont know about you. Get creative and show us who you are.   [sample vide link]</p>
-<input  required name="Instagram" placeholder="UPLOAD VIDEO HERE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mx-5"/>
+<input  required name="video" placeholder="UPLOAD VIDEO HERE" type="text" className="placeholder-black bg-inherit border border-black focus:outline-none p-2 mx-5" {...register('video')}/>
  <section className=" mt-5  mx-5">
    <span className="font-semibold">TIPS</span>
    <ul className="flex flex-col gap-2  tracking-wide">
@@ -210,10 +247,13 @@ a pony tail</li>
 fashion shoot.</li>
 <li>Be natural, no crazy poses or wild smiles</li>
 <li>On no occasion should you upload selfies or nudes</li>
+<li className="font-bold">click the submit button again when your file upload is 100%</li>
 </ul>
  </section>
 
- <button  type="submit" className="border bg-[#B4917E] uppercase font-bold rounded-md ml-5 py-3 px-5 mt-5"> Submit application</button>
+
+ <button  type="submit" className="border bg-[#B4917E] uppercase font-bold rounded-md ml-5 py-3 px-5 mt-5" > Submit application</button>
+ <p className="ml-4 mt-3">file upload {progress}%</p>
 </form>
      <div className="ml-4 mt-5 font-semibold text-green-900 font-fraunces">{message}</div>
 <Footer />
--- src/components/pages/InnerUpload1.js
+++ src/components/pages/InnerUpload1.js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 
 
 const InnerUpload1 = ({image, setImage}) => {
+
     const [preview, setPreview] = useState();
     
       const fileHandler = (event) => { 
@@ -35,7 +36,7 @@ const InnerUpload1 = ({image, setImage}) => {
     return ( <>  {preview ? (<img onClick={() => {setImage(null)}} className ="border border-dashed object-cover border-black h-[275px] w-3/4 md:w-1/4" src = {preview} alt = 'preview'/>) : ( <label className="border border-dashed border-black h-[275px] w-3/4 md:w-1/4">
     <div>
     <span className="p-3">#Photo *</span>
-    <input required  className="hidden"  accept="image/*" type="file" name="pic" id="pic" onChange={fileHandler}/>
+    <input   required  className="hidden"  accept="image/*" type="file" name="picture" id="pic" onChange={fileHandler}/>
     </div> 
   </label>  )} </>);
 }
